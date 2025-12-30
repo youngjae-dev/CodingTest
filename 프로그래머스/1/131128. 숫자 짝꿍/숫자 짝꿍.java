2062cbc8457679e6f56fdb4679bdf24dddeb9a57
Young Jae Shin <a01037497436@gmail.com>
2025-12-31 00:40:30 +0900

    Update 숫자 짝꿍.java 문제
    문제에 대한 고찰 내용 추가

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -1,5 +1,10 @@
 import java.util.*;
 
+// 이번 문제의 피드백
+// String + String 연산을 통해 answer에 누적하는 방식을 사용했었지만, 이 경우 String 객체의 특성에 의해 매번 새로운 객체가 생성되므로 메모리와 시간 소모가 심함
+// 이에 따라서 StringBuilder의 append 메소드를 사용해서 객체 하나에 계속해서 붙히도록 코드를 리팩토링
+
+
 class Solution {
     public String solution(String X, String Y) {
         int[] x = new int [10];
