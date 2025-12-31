14495c668e244ffac0b3a7895c1d378dff55a378
Young Jae Shin <a01037497436@gmail.com>
2025-12-31 13:41:27 +0900

    Update 서로 다른 부분 문자열의 개수.java
    문제에 대한 고찰 내용 추가

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
@@ -1,5 +1,9 @@
 import java.util.*;
 
+// 문제 고찰
+// HashMap와 StringBuilder를 사용했지만, HashMap보다는 HashSet을 사용하는것이 메모리를 덜 차지하기 때문에 변경하였고
+// StringBuilder는 substring을 사용하게 되면 어쩔 수 없이 객체가 매번 생성되기때문에 StringBuilder 객체를 따로 생성하지 않고 입력으로 들어온 str만을 사용하는 방식으로 코드를 변경하였다
+
 public class Main {
     public static int solution(String str){
         HashSet <String> set = new HashSet<> ();
@@ -16,4 +20,5 @@ public class Main {
         String str = input.nextLine();
         System.out.println(solution(str));
     }
+
 }
