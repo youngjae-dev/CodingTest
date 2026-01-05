82e0652b1b0b93f5512a7335643a142c6dbadddc
Young Jae Shin <a01037497436@gmail.com>
2026-01-05 15:05:20 +0900

    Update 전화번호 목록.java

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.util.*;
 
+// 문제에 대한 고찰 : 입력으로 들어오는 전화번호를 길이순이 아닌 사전순으로 배열함으로써 접두어 관계에 속하는 전화번호끼리 인접하도록 정렬한 후, startsWith로 접두어 여부를 확인하는 방법으로 코드를 리팩토링하였다.
+
 public class Main {
     public static String solution(int N, String[] numbers) {
         for(int i = 0; i < N - 1; ++i) {
@@ -21,4 +23,5 @@ public class Main {
             System.out.println(solution(N, numbers));
         }
     }
+
 }
