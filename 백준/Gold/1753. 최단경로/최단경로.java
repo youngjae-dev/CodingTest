d92dd32db098536ad17cd34b9ff26a988e2cfb58
Young Jae Shin <a01037497436@gmail.com>
2026-02-17 20:44:30 +0900

    [Gold IV] Title: 최단경로, Time: 820 ms, Memory: 111308 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -0,0 +1,78 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int V;
+    static ArrayList<Edge>[] edgeList;
+    static int[] dist;
+
+    static class Edge {
+        int vertex;
+        int dist;
+        Edge(int vertex, int dist){
+            this.vertex = vertex;
+            this.dist = dist;
+        }
+    }
+
+    static void dijkstra(int start) {
+        PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        q.offer(new int[] {start, 0});
+        dist[start] = 0;
+
+        while(!q.isEmpty()) {
+            int[] tmp = q.poll();
+            int num = tmp[0];
+            int sum = tmp[1];
+
+            if(dist[num] < sum) continue;
+
+            for(Edge e : edgeList[num]) {
+                if(dist[e.vertex] > dist[num] + e.dist) {
+                    dist[e.vertex] = dist[num] + e.dist;
+                    q.offer(new int[] {e.vertex, dist[e.vertex]});
+                }
+            }
+
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+
+        int K = Integer.parseInt(br.readLine());
+
+        dist = new int[V+1];
+        edgeList = new ArrayList[V+1];
+        for(int i = 0; i <= V; ++i) {
+            dist[i] = Integer.MAX_VALUE;
+            edgeList[i] = new ArrayList<>();
+        }
+
+        for(int i = 0; i < E; ++i) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            edgeList[u].add(new Edge(v, w));
+        }
+
+        dijkstra(K);
+
+        for(int i = 1; i <= V; ++i) {
+            int x = dist[i];
+            if(x == Integer.MAX_VALUE) {
+                System.out.print("INF");
+            }
+            else {
+                System.out.print(x);
+            }
+            System.out.println();
+        }
+    }
+}
