b809d8b57ff9a00e00375dd3c14be6d86cfb3eb7
Young Jae Shin <a01037497436@gmail.com>
2026-02-23 12:11:23 +0900

    [Gold IV] Title: N-Queen, Time: 2512 ms, Memory: 14636 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -2,45 +2,32 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[] dx = {-1, -1, -1};
-    static int[] dy = {0, -1, 1};
-    static boolean[][] visited;
+    static boolean[] colCheck = new boolean[15];
+    static boolean[] diagonalCheck = new boolean[30];
+    static boolean[] reverseDiagonalCheck = new boolean[30];
     static int N;
     static int answer = 0;
 
-    static void DFS(int r, int cnt) {
-        if(cnt == N) {
+    static void DFS(int r) {
+        if(r == N) {
             ++answer;
             return;
         }
         for(int i = 0; i < N; ++i) {
-            if(!visited[r][i] && checkBoard(r, i)) {
-                visited[r][i] = true;
-                DFS(r + 1, cnt + 1);
-                visited[r][i] = false;
-            }
+            if(colCheck[i] || diagonalCheck[r - i + N] || reverseDiagonalCheck[r + i]) continue;
+
+            colCheck[i] = diagonalCheck[r - i + N] = reverseDiagonalCheck[r + i] = true;
+            DFS(r+1);
+            colCheck[i] = diagonalCheck[r - i + N] = reverseDiagonalCheck[r + i] = false;
         }
-    }
-    static boolean checkBoard(int r, int c) {
-        for(int i = 0; i < 3; ++i) {
-            int nx = r + dx[i];
-            int ny = c + dy[i];
-            while(nx >= 0 && nx < N && ny >= 0 && ny < N) {
-                if(visited[nx][ny]) return false;
-                nx += dx[i];
-                ny += dy[i];
-            }
-        }
-        return true;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
-        visited = new boolean[N][N];
 
-        DFS(0, 0);
+        DFS(0);
         System.out.println(answer);
     }
 }
